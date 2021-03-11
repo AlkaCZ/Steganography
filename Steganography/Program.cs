4c2530c75f106dd2b6c8ab1f2fc296b928733d7b
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2021-03-11 12:57:22 +0100

    Možnost ovládat z CMD

--- Steganography/Program.cs
+++ Steganography/Program.cs
@@ -11,14 +11,12 @@ namespace Steganography
     {
         static void Main(string[] args)
         {
-            Bitmap bitmapImg = new Bitmap("Pic.png");
-            string msg = "Ello"; //Zprava
 
-
-            if (Console.ReadLine() == "start")
+            if (args.Contains("--hide"))
             {
 
-
+                string msg = args[2];
+                Bitmap bitmapImg = new Bitmap(args[3]);
                 for (int w = 0; w < bitmapImg.Width; w++)
                 {
                     for (int h = 0; h < bitmapImg.Height; h++)
@@ -31,43 +29,48 @@ namespace Steganography
                             int value = Convert.ToInt32(letter);
                             bitmapImg.SetPixel(w, h, Color.FromArgb(pixel.R, pixel.G, value));
                         }
-                       else if (w == bitmapImg.Width - 1 && h == bitmapImg.Height - 1)
+                        else if (w == bitmapImg.Width - 1 && h == bitmapImg.Height - 1)
                         {
                             bitmapImg.SetPixel(w, h, Color.FromArgb(pixel.R, pixel.G, msg.Length));
                         }
                     }
                 }
 
-                bitmapImg.Save("ConvertedPic.png");
+                bitmapImg.Save("ConvertedPic.bmp");
+                Console.WriteLine("DONE Converting");
+                Console.ReadLine();
             }
 
-            else
+            else if (args.Contains("--show"))
             {
-                Bitmap bitmapImgNew = new Bitmap("ConvertedPic.png");
-                string GivenMsg = "";
-                Color endPix = bitmapImgNew.GetPixel(bitmapImgNew.Width - 1, bitmapImgNew.Height - 1);
-                int GivenMsgLenght = endPix.B;
+                
 
-                for (int w = 0; w < bitmapImgNew.Width; w++)
                 {
-                    for (int h = 0; h < bitmapImgNew.Height; h++)
-                    {
-                        Color pixelEncr = bitmapImgNew.GetPixel(w, h);
+                    Bitmap bitmapImgNew = new Bitmap(args[2]);
+                    string GivenMsg = "";
+                    Color endPix = bitmapImgNew.GetPixel(bitmapImgNew.Width - 1, bitmapImgNew.Height - 1);
+                    int GivenMsgLenght = endPix.B;
 
-                        if (w < 1 && h < GivenMsgLenght)
+                    for (int w = 0; w < bitmapImgNew.Width; w++)
+                    {
+                        for (int h = 0; h < bitmapImgNew.Height; h++)
                         {
-                            int value = pixelEncr.B;
-                            char c = Convert.ToChar(value);
-                            string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
-                            GivenMsg += letter;
+                            Color pixelEncr = bitmapImgNew.GetPixel(w, h);
+
+                            if (w < 1 && h < GivenMsgLenght)
+                            {
+                                int value = pixelEncr.B;
+                                char c = Convert.ToChar(value);
+                                string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+                                GivenMsg += letter;
+                            }
                         }
                     }
+                    Console.WriteLine(GivenMsg);
                 }
-                Console.WriteLine(GivenMsg);
+                Console.WriteLine("DONE Reading");
+                Console.ReadLine();
             }
-            Console.WriteLine("DONE");
-            Console.ReadLine();
-
         }
     }
 }
